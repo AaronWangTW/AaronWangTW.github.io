671266d4b4e9687c018cc0652def90f34b8f657a
AaronWangTW <honglin2023@gmail.com>
2025-07-13 14:29:09 +0800

    new structure

--- personal-website/app/layout.js
+++ personal-website/app/layout.js
@@ -1,25 +1,18 @@
 import "./globals.css";
-import MainLayout from "./components/layout/mainLayout";
 
 export const metadata = {
   title: 'Your Personal Website',
   description: 'Welcome to my site!',
 };
 
-import { Felipa, Lovers_Quarrel, Roboto } from 'next/font/google';
-
-const felipa = Felipa({ subsets: ['latin'], weight: '400', variable: '--font-felipa' });
-const lovers = Lovers_Quarrel({ subsets: ['latin'], weight: '400', variable: '--font-lovers' });
-const roboto = Roboto({ subsets: ['latin'], weight: ['400', '700'], variable: '--font-roboto' });
-
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className={`${roboto.className} ${felipa.className} ${lovers.className}`}>
+    <html lang="en">
       <head>
         <title>Aaron Wang</title>
       </head>
-      <body className="font-felipa">
-        <MainLayout>{children}</MainLayout>
+      <body>
+        {children}
       </body>
     </html>
   );
--- personal-website/app/page.js
+++ personal-website/app/page.js
@@ -2,7 +2,6 @@ export default function HomePage() {
   return (
     <div className="text-gray-700">
       <h1 className="text-3xl font-bold mb-4">Welcome!</h1>
-      <p>Use the prompt on the left to explore my site.</p>
     </div>
   );
 }
